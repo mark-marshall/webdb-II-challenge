f2b1bcbfcf28e03d7f769a0282567cf4c753005b
mark-marshall <mark.marshallgp@gmail.com>
2019-03-26 12:51:28 +0000

    [MVP] Added basic CRUD for zoos

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .then(zoo => {
+      res.status(200).json(zoo);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
